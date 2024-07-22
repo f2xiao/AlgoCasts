fd2e08d16d51c9cb3271897770a2d49535bad4f7
fangfang <25191617+f2xiao@users.noreply.github.com>
2024-07-22 16:07:24 -0400

    implement queque class from stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,52 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    // use another stack as a helper data structure?
+    this.second = new Stack();
+  }
+
+  add(record) {
+    this.first.push(record);
+  }
+
+  remove() {
+    while (this.first.peek()) {
+      const record = this.first.pop();
+      this.second.push(record);
+    }
+
+    const lastItem = this.second.pop();
+
+    // restore the first and the second stack
+    while (this.second.peek()) {
+      const record = this.second.pop();
+      this.first.push(record);
+    }
+
+    return lastItem;
+  }
+
+  peek() {
+    while (this.first.peek()) {
+      const record = this.first.pop();
+      this.second.push(record);
+    }
+
+    const firstItem = this.second.peek();
+
+    // restore the first and the second stack
+    while (this.second.peek()) {
+      const record = this.second.pop();
+      this.first.push(record);
+    }
+
+    return firstItem;
+  }
+}
 
 module.exports = Queue;
