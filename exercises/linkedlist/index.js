f7ca8bebe2c3fe715bb674999964ba44e8dcc0d1
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-07-24 14:10:11 -0400

    add Node class and LinkedList class

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,37 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  insertFirst(data) {
+    if (!this.head) {
+      this.head = new Node(data);
+    } else {
+      const n2 = new Node(data, this.head);
+      this.head = n2;
+    }
+
+    this.length++;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+}
 
 module.exports = { Node, LinkedList };
