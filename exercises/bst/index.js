2a63ed18543c89ac94846c17cf47961ff2e28057
Fangfang <25191617+f2xiao@users.noreply.github.com>
2024-10-10 15:26:25 -0400

    add contains method for bst

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -30,6 +30,21 @@ class Node {
         }
 
     }
+
+    contains(data){
+        if(data< this.data && this.left){
+            return this.left.contains(data)
+        }else if(data > this.data && this.right){
+            return this.right.contains(data)
+        }
+
+        if(data === this.data){
+            return this
+        }
+
+        return null
+    }
 }
 
 module.exports = Node;
+ 
